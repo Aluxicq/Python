5ef48c22a635e283cd86d5e55eb750f3b63a1f73
Aleksei <alexmamleev1@gmail.com>
2023-12-12 16:05:21 +0500

    Aluxic11

--- Python/AluxicCode.py
+++ Python/AluxicCode.py
@@ -1,2 +1,26 @@
-print('Hello, world!')
-print('fdfdfdf')
+import random
+
+attempts = 3
+print("Попробуй угадать число от 0 до 5!")
+number = random.randint(0, 5)
+while attempts != 0:
+
+    user_number = int(input("Введи число: "))
+    if user_number == number:
+        print("Это верно, я загадал число", number)
+        break
+
+    else:
+        print('Не угадал')
+        attempts -= 1
+    if attempts == 0:
+        print('Ты проиграл!')
+    if attempts == 1 and number >= 3:
+        print('Даю тебе подсказку - число больше или равно трем')
+    if attempts == 1 and number < 3:
+        print('Даю тебе подсказку - число меньше трех')
+
+
+
+
+
